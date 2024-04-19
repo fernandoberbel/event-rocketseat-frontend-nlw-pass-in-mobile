6d0ba5267d27ae572929c43046267c6afac8b52d
fernandoberbel <fberbel1000@gmail.com>
2024-04-18 22:50:47 -0300

    feat: add api, persistence storage, global state and animation

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -8,9 +8,14 @@ import {
   TouchableOpacity,
   Alert,
   Modal,
+  Share,
 } from "react-native";
+import { MotiView } from "moti";
 import { FontAwesome } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
+import { Redirect } from "expo-router";
+
+import { useBadgeStore } from "@/store/badge-store";
 
 import { colors } from "@/styles/colors";
 
@@ -20,8 +25,22 @@ import { Button } from "@/components/button";
 import { QRCode } from "@/components/qrcode";
 
 export default function Ticket() {
-  const [image, setImage] = useState("");
   const [expandQRCode, setExpandQRCode] = useState(false);
+
+  const badgeStore = useBadgeStore();
+
+  async function handleShare() {
+    try {
+      if (badgeStore.data?.checkInURL) {
+        await Share.share({
+          message: badgeStore.data.checkInURL,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Compartilhar", "Não foi possivel compartilhar.");
+    }
+  }
 
   async function handleSelectImage() {
     try {
@@ -32,12 +51,16 @@ export default function Ticket() {
       });
 
       if (result.assets) {
-        setImage(result.assets[0].uri);
+        badgeStore.updateAvatar(result.assets[0].uri);
       }
     } catch (error) {
       console.log(error);
       Alert.alert("Foto", "Não foi possível selecionar a imagem.");
     }
+  }
+
+  if (!badgeStore.data?.checkInURL) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -51,29 +74,48 @@ export default function Ticket() {
         showsVerticalScrollIndicator={false}
       >
         <Credential
-          image={image}
+          data={badgeStore.data}
           onChangeAvatar={handleSelectImage}
           onExpandQRCode={() => setExpandQRCode(true)}
         />
 
-        <FontAwesome
-          name="angle-double-down"
-          size={24}
-          color={colors.gray[300]}
-          className="self-center my-6"
-        />
+        <MotiView
+          from={{
+            translateY: 0,
+          }}
+          animate={{
+            translateY: 10,
+          }}
+          transition={{
+            loop: true,
+            type: "timing",
+            duration: 700, //milessegundos
+          }}
+        >
+          <FontAwesome
+            name="angle-double-down"
+            size={24}
+            color={colors.gray[300]}
+            className="self-center my-6"
+          />
+        </MotiView>
 
         <Text className="text-white font-bold text-2xl mt-4">
           Compartilhar credencial
         </Text>
 
         <Text className="text-white font-regular text-base mt-1 mb-6">
-          Mostre ao mundo que você vai participar do Unite Summit!
+          Mostre ao mundo que você vai participar do evento{" "}
+          {badgeStore.data.eventTitle}!
         </Text>
 
-        <Button title="Compartilhar" />
+        <Button title="Compartilhar" onPress={handleShare} />
 
-        <TouchableOpacity activeOpacity={0.7} className="mt-10">
+        <TouchableOpacity
+          className="mt-10"
+          activeOpacity={0.7}
+          onPress={() => badgeStore.remove()}
+        >
           <Text className="text-base text-white font-bold text-center">
             Remover Ingresso
           </Text>
